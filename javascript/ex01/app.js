67ed644a9efe26cc834f3cef6418f76edd858e59
zyoonshin <dwsrh03@gmail.com>
2022-12-06 18:11:36 +0900

    bitcamp

---
+++ javascript/ex01/app.js
@@ -0,0 +1,26 @@
+const { fail } = require('assert');
+const express = require('express');
+const { resolve } = require('path');
+const app = express()
+const port = 3000
+
+
+// await 위에는 항상 async
+
+app.get('/exam05_1', async (req, res) => {
+  await new Promise(resolve => setTimeout(resolve,10000));
+  res.send('console.log("exam05_1 ok!");');
+})
+
+app.get('/exam05_2', (req, res) => {
+  res.send('console.log("exam05_2 ok!");');
+})
+
+app.get('/exam05_x', async (req, res) => {
+  await new Promise(resolve => setTimeout(resolve,10000));  // resolve가 서포터가 수행할 약속된 작업
+  res.send('var rate = 30000;')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
