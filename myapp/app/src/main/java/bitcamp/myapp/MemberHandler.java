6460900fa9feed0e9f991decb2c3964979ff7b0d
zyoonshin <dwsrh03@gmail.com>
2023-01-05 16:28:33 +0900

    myapp

---
+++ myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
@@ -0,0 +1,49 @@
+package bitcamp.myapp;
+
+import java.sql.Date;
+
+public class MemberHandler {
+
+  static final int SIZE = 100;
+  static int count = 0;
+
+  // 레퍼런스 배열 준비
+  static Member[] members = new Member[SIZE];
+
+  static void inputMember() {
+
+    Member m = new Member();
+    m.no = Prompt.inputInt("번호? ");
+    m.name = Prompt.inputString("이름? ");
+    m.tel = Prompt.inputString("전화? ");
+    m.postNo = Prompt.inputString("우편번호? ");
+    m.basicAddress = Prompt.inputString("주소1? ");
+    m.detailAddress = Prompt.inputString("주소2? ");
+    m.working = Prompt.inputInt("0. 미취업\n1. 재직중\n재직자? ") == 1;
+    m.gender = Prompt.inputInt("0. 남자\n1. 여자\n성별? ") == 0 ? 'M' : 'W';
+    m.level = (byte) Prompt.inputInt("0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ");
+    m.createdDate = new Date(System.currentTimeMillis()).toString();
+
+    members[count++] = m;
+
+  }
+
+  static void printMembers() {
+    System.out.println("번호\t이름\t전화\t재직\t전공");
+    for (int i = 0; i < count; i++) {
+      Member m = members[i];
+
+      String levelTitle;
+      switch (m.level) {
+        case 0: levelTitle = "비전공자"; break;
+        case 1: levelTitle = "준전공자"; break;
+        default: levelTitle = "전공자";
+      }
+
+      System.out.printf("%d\t%s\t%s\t%s\t%s\n",
+          m.no, m.name, m.tel,
+          m.working ? "예" : "아니오",
+              levelTitle);
+    }
+  }
+}
--- myapp/app/src/main/java/bitcamp/myapp/Prompt.java
+++ myapp/app/src/main/java/bitcamp/myapp/Prompt.java
@@ -3,18 +3,18 @@ package bitcamp.myapp;
 import java.util.Scanner;
 
 public class Prompt {
-
   static Scanner scanner = new Scanner(System.in);
 
-  static String promptString(String title) {
-    System.out.println(title);
+  static String inputString(String title) {
+    System.out.print(title);
     return scanner.nextLine();
   }
 
-  static int promptInt(String title) {
-    return Integer.parseInt(promptString(title));
+  static int inputInt(String title) {
+    return Integer.parseInt(inputString(title));
   }
 
+  // Prompt 클래스를 다 사용한 후에 자원을 해제시킬 수 있는 메서드를 추가한다.
   static void close() {
     scanner.close();
   }
