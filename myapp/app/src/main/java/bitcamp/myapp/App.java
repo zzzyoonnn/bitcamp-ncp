6e666e9a723460470ddce1c3b064936ea6251b7b
zyoonshin <dwsrh03@gmail.com>
2023-01-05 20:17:02 +0900

    java

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,33 +4,31 @@ public class App {
 
   public static void main(String[] args) {
 
-    // 1단계
-    System.out.println("1. 회원관리");
-    System.out.println("9. 종료");
+    goMenu();
+
+    Prompt.close();
 
 
-
-
-    // 2단계
-
-    System.out.println("1. 등록");
-    System.out.println("2. 목록");
-    System.out.println("3. 조회");
-    System.out.println("4. 변경");
-    System.out.println("5. 삭제");
-    System.out.println("0. 이전");
-
-
-
-
-
-
-    MemberHandler.inputMembers();
-
-    System.out.println();
-
-    MemberHandler.printMembers();
   } // main()
+  public static void goMenu() {
+    while (true) {
+      System.out.println("1.회원관리");
+      System.out.println("9. 종료");
+
+      int menuNo = Prompt.inputInt("메뉴>");
+
+      if (menuNo == 1) {
+        MemberHandler.service();
+      } else if (menuNo == 9 ) {
+        break;
+      } else {
+        System.out.println("잘못입력");
+      }
+    }
+
+  }
+
+
 
 } // class App
 
--- myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
@@ -8,41 +8,57 @@ public class MemberHandler {
   // 레퍼런스 배열 준비
   static Member[] members = new Member[SIZE];
 
-
-  static void inputMembers() {
+  static void inputMember() {
     Member m = new Member();
     m.no = Prompt.inputInt("번호? ");
     m.name = Prompt.inputString("이름? ");
     m.tel = Prompt.inputString("전화? ");
     m.working = Prompt.inputInt("0. 미취업\n1. 재직중\n재직자? ") == 1;
-    m.gender = Prompt.inputInt("0. 남자\n1. 여자\n성별? ") == 0 ? 'M' : 'W';
     m.level = (byte) Prompt.inputInt("0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ");
-
-    // 지금 금방 만든 객체에 사용자가 입력한 값을 저장한 후
-    // 그 객체의 주소를 잃어버리지 않게 레퍼런스 배열에 보관해 둔다.
     members[count++] = m;
 
 
-    Prompt.close();
   }
+  public static void service() {
 
+    while (true) {
+      System.out.println("1. 등록");
+      System.out.println("2. 목록");
+      System.out.println("3. 조회");
+      System.out.println("4. 변경");
+      System.out.println("5. 삭제");
+      System.out.println("6. 등록");
+      int menuNo = Prompt.inputInt("회원관리");
+
+      if (menuNo == 0) {
+        break;
+      } else if (menuNo == 1) {
+        inputMember();
+      } else if (menuNo == 2) {
+        printMembers();
+      } else {
+        System.out.println("잘못 입력");
+      }
+    }
+
+  }
   static void printMembers() {
 
-    System.out.println("번호\t이름\t전화\t재직\t성별\t전공");
+    System.out.println("번호\t이름\t전화\t재직\t전공");
 
     for (int i = 0; i < count; i++) {
-      Member m = new Member();
-      m = members[i];
+      Member m = members[i];
+
       String levelTitle;
       switch (m.level) {
         case 0: levelTitle = "비전공자"; break;
         case 1: levelTitle = "준전공자"; break;
         default: levelTitle = "전공자";
       }
-      System.out.printf("%d\t%s\t%s\t%s\t%s\t%s\n",
-          m.no, m.name, m.tel,
-          m.working ? "예" : "아니오",
-              m.gender == 'M' ? "남자" : "여자", levelTitle);
+
+      System.out.printf("%d\t%s\t%s\t%s\t%s\n",
+          m.no,m.name,m.tel,
+          m.working ? "예" : "아니오",levelTitle);
     }
   }
 }
