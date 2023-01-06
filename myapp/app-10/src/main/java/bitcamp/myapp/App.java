e20b6bb254e95eba0bd069c7cbc7341723bf470c
zyoonshin <dwsrh03@gmail.com>
2023-01-06 18:18:39 +0900

    app

--- myapp/app-10/src/main/java/bitcamp/myapp/App.java
+++ myapp/app-10/src/main/java/bitcamp/myapp/App.java
@@ -5,35 +5,48 @@ public class App {
   public static void main(String[] args) {
     goMainMenu();
     System.out.println("안녕히 가세요!");
-
-    // 프로그램이 사용한 자원 해제하기
+    
     Prompt.close();
+
   } // main()
+  public static void goMenu() {
+    while (true) {
+      System.out.println("1.회원관리");
+      System.out.println("9. 종료");
+
+      int menuNo = Prompt.inputInt("메뉴>");
+
+      if (menuNo == 1) {
+        MemberHandler.service();
+      } else if (menuNo == 9 ) {
+        break;
+      } else {
+        System.out.println("잘못입력");
+      }
+    }
+
+  }
+
+
 
   private static void goMainMenu() {
-	  
-	// 일반 회원 목록을 저장할 메모리를 준비한다.
-	MemberHandler generalMemberHandler = new MemberHandler("일반학생");
-	
-	// 국비지원 목록을 저장할 메모리를 준비한다.
-	MemberHandler supportedMemberHandler = new MemberHandler("국비지원학생");
-		
-	// 기업 위탁 교육생 목록을 저장할 메모리를 준비한다.
-	MemberHandler companyMemberHandler = new MemberHandler("위탁교육생");
-	  
     while (true) {
       System.out.println("1. 일반학생관리");
       System.out.println("2. 국비지원학생관리");
       System.out.println("3. 위탁교육생관리");
+      
       System.out.println("9. 종료");
       int menuNo = Prompt.inputInt("메뉴> ");
 
       if (menuNo == 1) {
-    	  generalMemberHandler.service();
-      } else if (menuNo == 2) {
-    	  supportedMemberHandler.service();
-      } else if (menuNo == 3) {
-    	  companyMemberHandler.service();
+    	  MemberHandler.title = "일반학생";
+        MemberHandler.service();
+      } else if (menuNo ==2) {
+    	  Member2Handler.title = "국비지원학생";
+    	  Member2Handler.service();
+      } else if (menuNo ==3) {
+    	  Member3Handler.title = "위탁교육생";
+    	  Member3Handler.service();
       } else if (menuNo == 9) {
         break;
       } else {
