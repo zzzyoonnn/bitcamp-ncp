7eb50b9fbf2e9dfd5ccb4cee2e7c2d8e7eef08f4
zyoonshin <dwsrh03@gmail.com>
2023-03-16 17:38:33 +0900

    .

--- myapp/app-server/src/main/resources/static/temp/src/test.js
+++ myapp/app-server/src/main/resources/static/temp/src/test.js
@@ -1,34 +1,175 @@
-"use strict";
+HTML CSS BabelResult Skip Results Iframe
+EDIT ON
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
+}
 
-// 비제어 컴포넌트 - 파일 입력 태그
-// => <input type="file" />은 항상 비제어 컴포넌트다.
-// => 프로그래밍으로 값을 다룰 수 없다.
-//
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+      product.name :
+      <span style={{color: 'red'}}>
+        {product.name}
+      </span>;
 
-class FileInput extends React.Component {
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
+      );
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+
   constructor(props) {
     super(props);
-    this.fileInput = React.createRef();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    alert(`Selected file:  ${this.fileInput.current.files[0].name}`);
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Upload file:
-          <input type="file" ref={this.fileInput} />
-        </label>
-        <input type="submit" value="Submit" />
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          {' '}
+          Only show products in stock
+        </p>
       </form>
     );
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FileInput />);
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+  
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+const root = ReactDOM.createRoot(document.getElementById('container'));
+root.render(<FilterableProductTable products={PRODUCTS} />);
+View Compiled
+
+Resources
--- myapp/app-server/src/main/resources/static/temp/test.js
+++ myapp/app-server/src/main/resources/static/temp/test.js
@@ -1,10 +1,3 @@
-"use strict";
-
-// 비제어 컴포넌트 - 파일 입력 태그
-// => <input type="file" />은 항상 비제어 컴포넌트다.
-// => 프로그래밍으로 값을 다룰 수 없다.
-//
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -13,44 +6,236 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var FileInput = function (_React$Component) {
-  _inherits(FileInput, _React$Component);
+var ProductCategoryRow = function (_React$Component) {
+  _inherits(ProductCategoryRow, _React$Component);
 
-  function FileInput(props) {
-    _classCallCheck(this, FileInput);
+  function ProductCategoryRow() {
+    _classCallCheck(this, ProductCategoryRow);
 
-    var _this = _possibleConstructorReturn(this, (FileInput.__proto__ || Object.getPrototypeOf(FileInput)).call(this, props));
-
-    _this.fileInput = React.createRef();
-    _this.handleSubmit = _this.handleSubmit.bind(_this);
-    return _this;
+    return _possibleConstructorReturn(this, (ProductCategoryRow.__proto__ || Object.getPrototypeOf(ProductCategoryRow)).apply(this, arguments));
   }
 
-  _createClass(FileInput, [{
-    key: "handleSubmit",
-    value: function handleSubmit(event) {
-      event.preventDefault();
-      alert("Selected file:  " + this.fileInput.current.files[0].name);
-    }
-  }, {
+  _createClass(ProductCategoryRow, [{
     key: "render",
     value: function render() {
+      var category = this.props.category;
       return React.createElement(
-        "form",
-        { onSubmit: this.handleSubmit },
+        "tr",
+        null,
         React.createElement(
-          "label",
-          null,
-          "Upload file:",
-          React.createElement("input", { type: "file", ref: this.fileInput })
-        ),
-        React.createElement("input", { type: "submit", value: "Submit" })
+          "th",
+          { colSpan: "2" },
+          category
+        )
       );
     }
   }]);
 
-  return FileInput;
+  return ProductCategoryRow;
 }(React.Component);
 
-var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(FileInput, null));
+var ProductRow = function (_React$Component2) {
+  _inherits(ProductRow, _React$Component2);
+
+  function ProductRow() {
+    _classCallCheck(this, ProductRow);
+
+    return _possibleConstructorReturn(this, (ProductRow.__proto__ || Object.getPrototypeOf(ProductRow)).apply(this, arguments));
+  }
+
+  _createClass(ProductRow, [{
+    key: "render",
+    value: function render() {
+      var product = this.props.product;
+      var name = product.stocked ? product.name : React.createElement(
+        "span",
+        { style: { color: "red" } },
+        product.name
+      );
+
+      return React.createElement(
+        "tr",
+        null,
+        React.createElement(
+          "td",
+          null,
+          name
+        ),
+        React.createElement(
+          "td",
+          null,
+          product.price
+        )
+      );
+    }
+  }]);
+
+  return ProductRow;
+}(React.Component);
+
+var ProductTable = function (_React$Component3) {
+  _inherits(ProductTable, _React$Component3);
+
+  function ProductTable() {
+    _classCallCheck(this, ProductTable);
+
+    return _possibleConstructorReturn(this, (ProductTable.__proto__ || Object.getPrototypeOf(ProductTable)).apply(this, arguments));
+  }
+
+  _createClass(ProductTable, [{
+    key: "render",
+    value: function render() {
+      var filterText = this.props.filterText;
+      var inStockOnly = this.props.inStockOnly;
+
+      var rows = [];
+      var lastCategory = null;
+
+      this.props.products.forEach(function (product) {
+        if (product.name.indexOf(filterText) === -1) {
+          return;
+        }
+        if (inStockOnly && !product.stocked) {
+          return;
+        }
+        if (product.category !== lastCategory) {
+          rows.push(React.createElement(ProductCategoryRow, {
+            category: product.category,
+            key: product.category
+          }));
+        }
+        rows.push(React.createElement(ProductRow, { product: product, key: product.name }));
+        lastCategory = product.category;
+      });
+
+      return React.createElement(
+        "table",
+        null,
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            null,
+            React.createElement(
+              "th",
+              null,
+              "Name"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Price"
+            )
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          rows
+        )
+      );
+    }
+  }]);
+
+  return ProductTable;
+}(React.Component);
+
+var SearchBar = function (_React$Component4) {
+  _inherits(SearchBar, _React$Component4);
+
+  function SearchBar() {
+    _classCallCheck(this, SearchBar);
+
+    return _possibleConstructorReturn(this, (SearchBar.__proto__ || Object.getPrototypeOf(SearchBar)).apply(this, arguments));
+  }
+
+  _createClass(SearchBar, [{
+    key: "render",
+    value: function render() {
+      var filterText = this.props.filterText;
+      var inStockOnly = this.props.inStockOnly;
+
+      return React.createElement(
+        "form",
+        null,
+        React.createElement("input", { type: "text", placeholder: "Search...", value: filterText }),
+        React.createElement(
+          "p",
+          null,
+          React.createElement("input", { type: "checkbox", checked: inStockOnly }),
+          "Only show products in stock"
+        )
+      );
+    }
+  }]);
+
+  return SearchBar;
+}(React.Component);
+
+var FilterableProductTable = function (_React$Component5) {
+  _inherits(FilterableProductTable, _React$Component5);
+
+  function FilterableProductTable(props) {
+    _classCallCheck(this, FilterableProductTable);
+
+    var _this5 = _possibleConstructorReturn(this, (FilterableProductTable.__proto__ || Object.getPrototypeOf(FilterableProductTable)).call(this, props));
+
+    _this5.state = {
+      filterText: "",
+      inStockOnly: false
+    };
+    return _this5;
+  }
+
+  _createClass(FilterableProductTable, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(SearchBar, {
+          filterText: this.state.filterText,
+          inStockOnly: this.state.inStockOnly
+        }),
+        React.createElement(ProductTable, {
+          products: this.props.products,
+          filterText: this.state.filterText,
+          inStockOnly: this.state.inStockOnly
+        })
+      );
+    }
+  }]);
+
+  return FilterableProductTable;
+}(React.Component);
+
+var PRODUCTS = [{
+  category: "Sporting Goods",
+  price: "$49.99",
+  stocked: true,
+  name: "Football"
+}, {
+  category: "Sporting Goods",
+  price: "$9.99",
+  stocked: true,
+  name: "Baseball"
+}, {
+  category: "Sporting Goods",
+  price: "$29.99",
+  stocked: false,
+  name: "Basketball"
+}, {
+  category: "Electronics",
+  price: "$99.99",
+  stocked: true,
+  name: "iPod Touch"
+}, {
+  category: "Electronics",
+  price: "$399.99",
+  stocked: false,
+  name: "iPhone 5"
+}, { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }];
+
+var root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(React.createElement(FilterableProductTable, { products: PRODUCTS }));
